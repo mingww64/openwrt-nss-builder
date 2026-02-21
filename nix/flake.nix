82e712b15594c84bf024912d343e5c8e08948487
mingww64 <me@lcia.eu.org>
2026-02-20 16:22:19 -0800

    Update package syncing method and add packages.txt for static package list

--- nix/flake.nix
+++ nix/flake.nix
@@ -39,22 +39,23 @@
 
       buildScript = pkgs.writeShellScriptBin "build-nss-image" ''
         set -e
-        PACKAGES=""
+        PACKAGES=$(cat ./packages.txt || echo "")
         if [ "$1" == "--sync-packages" ]; then
           LOGFILE="../build-$(date +%Y%m%d-%H%M%S)-sync.log"
-          echo "--- Syncing package list from router using apk ---" | tee -a "$LOGFILE"
+          echo "--- Syncing package list from router using ssh ---" | tee -a "$LOGFILE"
 
           # Merge static package list and ssh apk info, remove duplicates
-          STATIC_PACKAGES="acme acme-acmesh acme-common apk-mbedtls avahi-nodbus-daemon busybox ca-bundle certtool cgi-io collectd collectd-mod-cpu collectd-mod-interface collectd-mod-iwinfo collectd-mod-load collectd-mod-memory collectd-mod-network collectd-mod-rrdtool curl dawn dbus ddns-scripts ddns-scripts-cloudflare ddns-scripts-services dnsmasq dropbear e2fsprogs etherwake firewall4 fstools fwtool getrandom hostapd-common ip-tiny iperf3 ipq-wifi-linksys_mr7350 iw jansson4 jshn jsonfilter libatomic1 libavahi-nodbus-support libblkid1 libblobmsg-json20251208 libdaemon libdbus libe2p2 libev libexpat libext2fs2 libgcrypt libgmp10 libgnutls libgpg-error libiperf3 libiwinfo-data libiwinfo20230701 libjson-c5 libjson-script20251208 libltdl7 liblua5.1.5 liblucihttp-lua liblucihttp-ucode liblucihttp0 libmbedtls21 libmnl0 libncurses6 libnettle8 libnftnl11 libnghttp2-14 libnl-tiny1 libopenssl-conf libopenssl3 libpcap1 libpcre2 libprotobuf-c libpthread libreadline8 librrd1 librt libsmartcols1 libss2 libstdcpp6 libubox20251208 libubus-lua libubus20251202 libuci20250120 libuclient20201210 libucode20230711 libudebug libustream-mbedtls20201210 libuuid1 libuv1 libwebsockets-full libxml2-16 logd losetup lua luci luci-app-acme luci-app-dawn luci-app-ddns luci-app-filemanager luci-app-firewall luci-app-ocserv luci-app-package-manager luci-app-statistics luci-app-tailscale-community luci-app-ttyd luci-app-upnp luci-app-wol luci-base luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-lib-uqr luci-light luci-lua-runtime luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-nginx luci-proto-ipv6 luci-proto-ppp luci-theme-argon luci-theme-bootstrap mdns-repeater miniupnpd-nftables mtd netifd nftables-json nginx-mod-luci nginx-mod-ubus nginx-ssl nginx-ssl-util ocserv odhcp6c odhcpd-ipv6only openssl-util openwrt-keyring ppp ppp-mod-pppoe procd procd-seccomp procd-ujail resolveip rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci rpcd-mod-rrdns rpcd-mod-ucode rrdtool1 socat tailscale tcpdump terminfo ttyd ubi-utils uboot-envtools ubox ubus ubusd uci uclient-fetch ucode ucode-mod-digest ucode-mod-fs ucode-mod-html ucode-mod-log ucode-mod-lua ucode-mod-math ucode-mod-nl80211 ucode-mod-rtnl ucode-mod-ubus ucode-mod-uci ucode-mod-uloop udp-broadcast-relay-redux uhttpd uhttpd-mod-ubus umdns urandom-seed urngd usign uwsgi uwsgi-cgi-plugin uwsgi-luci-support uwsgi-syslog-plugin wget-ssl wifi-scripts wireless-regdb wpad-mbedtls zlib"
           SSH_PACKAGES=$(ssh ${routerUser}@${routerIp} "apk info" | grep -vE '(kmod|kernel|base-files|libc|libgcc|qca-nss|nss-dp|ath11k)')
-          PACKAGES=$(echo "$STATIC_PACKAGES $SSH_PACKAGES" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-          echo -e "--- Including packages: --- \n $PACKAGES \n--- End of package list ---" | tee -a "$LOGFILE"
+
+          PACKAGES=$(echo "$PACKAGES $SSH_PACKAGES" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+        else
+          LOGFILE="../build-$(date +%Y%m%d-%H%M%S).log"
         fi
-        
+        echo -e "--- Including packages: --- \n $PACKAGES \n--- End of package list ---" | tee -a "$LOGFILE"
+
         LOGFILE="../build-$(date +%Y%m%d-%H%M%S).log"
         # Generate config fragment to enable all packages
         cd source
-        CONFIG_FRAGMENT=""
         if [ -n "$PACKAGES" ]; then
           for pkg in $PACKAGES; do
             # Only use valid OpenWrt package names
